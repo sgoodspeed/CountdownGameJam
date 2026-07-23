aaad7583e25f9752f620d6882e1097abb69b19de
pblieka <91687441+pblieka@users.noreply.github.com>
2026-07-23 19:06:45 -0400

    feat: hour markers surrounding clock

---
+++ Assets/Scenes/GameScene/Environment/HourMarker.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Countdown
+{
+    public class HourMarker : MonoBehaviour
+    {
+        [SerializeField] private int hour;
+        [SerializeField] private SpriteRenderer sprite;
+        
+        private void Update()
+        {
+        }
+    }
+}
