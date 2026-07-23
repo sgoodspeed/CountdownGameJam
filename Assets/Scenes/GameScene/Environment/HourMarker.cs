6e2961ba63ed6cfb0d63bbe4b506c768cc84f287
pblieka <91687441+pblieka@users.noreply.github.com>
2026-07-23 19:14:59 -0400

    feat: hour markers animate

--- Assets/Scenes/GameScene/Environment/HourMarker.cs
+++ Assets/Scenes/GameScene/Environment/HourMarker.cs
@@ -7,8 +7,34 @@ namespace Countdown
         [SerializeField] private int hour;
         [SerializeField] private SpriteRenderer sprite;
         
+        [SerializeField] private Color startColor;
+        [SerializeField] private Color endColor;
+
         private void Update()
         {
+            UpdateAlpha();
+        }
+
+        private void UpdateAlpha()
+        {
+            GameState gameState = GameState.Instance;
+            int currentHour = gameState.GetCurrentHour() + 1;
+
+            float alpha;
+            if (currentHour > hour)
+            {
+                alpha = 1f;
+            }
+            else if (currentHour < hour)
+            {
+                alpha = 0f;
+            }
+            else
+            {
+                alpha = gameState.GetCurrentHourProgress();
+            }
+
+            sprite.color = Color.Lerp(startColor, endColor, alpha);
         }
     }
 }
