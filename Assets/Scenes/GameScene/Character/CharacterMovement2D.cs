f5d3ec60f1c4f00624c85be6a7ad6c52830b84d2
pblieka <91687441+pblieka@users.noreply.github.com>
2026-07-23 21:38:47 -0400

    feat: better 2d character collision

--- Assets/Scenes/GameScene/Character/CharacterMovement2D.cs
+++ Assets/Scenes/GameScene/Character/CharacterMovement2D.cs
@@ -13,24 +13,31 @@ namespace Countdown
     public class CharacterMovement2D : MonoBehaviour
     {
         [SerializeField] private Rigidbody2D body;
+        [SerializeField] private Collider2D collision;
+        [SerializeField] private float skinDistance = 0.1f;
+        
         [SerializeField] private float moveSpeed = 5f;
+        
+        private readonly RaycastHit2D[] hits = new RaycastHit2D[10];
 
         private void Awake()
         {
-            body.bodyType = RigidbodyType2D.Dynamic;
-            body.gravityScale = 0f;
             body.freezeRotation = true;
         }
 
         private void FixedUpdate()
         {
             Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-            if (input.sqrMagnitude > 1f)
-            {
-                input.Normalize();
-            }
+            if (input.sqrMagnitude > 1f) { input.Normalize(); }
 
-            body.linearVelocity = input * moveSpeed;
+            var distance = moveSpeed * Time.deltaTime;
+            var direction = input.normalized * (distance);
+            var hitCount = collision.Cast(direction, hits, direction.magnitude + Mathf.Epsilon);
+            for (var i = 0; i < hitCount; i++) {
+                distance = Mathf.Min(distance, hits[i].distance - skinDistance);
+            }
+            
+            body.MovePosition(body.position + direction * distance);
         }
     }
 }
