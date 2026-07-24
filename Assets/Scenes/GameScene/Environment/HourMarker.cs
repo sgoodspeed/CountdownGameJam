02e1af9cb2643019a768972e354b156195340141
pblieka <91687441+pblieka@users.noreply.github.com>
2026-07-24 17:12:08 -0400

    feat: Hour text on pillars

--- Assets/Scenes/GameScene/Environment/HourMarker.cs
+++ Assets/Scenes/GameScene/Environment/HourMarker.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 namespace Countdown
@@ -6,13 +7,41 @@ namespace Countdown
     {
         [SerializeField] private int hour;
         [SerializeField] private SpriteRenderer sprite;
-        
+        [SerializeField] private TMP_Text hourText;
+
         [SerializeField] private Color startColor;
         [SerializeField] private Color endColor;
+
+        private static readonly (int Value, string Numeral)[] RomanNumerals =
+        {
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I"),
+        };
+
+        private void Awake()
+        {
+            hourText.text = ToRomanNumeral(hour);
+        }
 
         private void Update()
         {
             UpdateAlpha();
+        }
+
+        private static string ToRomanNumeral(int number)
+        {
+            var result = new System.Text.StringBuilder();
+            foreach (var (value, numeral) in RomanNumerals)
+            {
+                while (number >= value)
+                {
+                    result.Append(numeral);
+                    number -= value;
+                }
+            }
+
+            return result.ToString();
         }
 
         private void UpdateAlpha()
